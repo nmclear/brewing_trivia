c62eb79698a83d8461adda75c86d7a96ba1c1967
Nick Clear <nickclear22@gmail.com>
2018-05-18 00:07:36 -0700

    first attempt at cleaning up code (DRY)

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
     //  Variable to hold setInterval that runs timer
     var intervalId;
     var timerRunning = false;
-
     var SECOND = 1000;
-    var time = 30;
 
+    //result variables
     var totalCorrect = 0;
     var totalWrong = 0;
     var totalNoAnswer = 0;
 
+    //tracks what question in questionBankArr
     var questionNum = 0;
 
 //========================================================================================================================
@@ -41,7 +41,7 @@ var questionBankArr = [
 
     question3 = {
         question: "What is the largest craft brewery in the United States?",
-        correctAnswer: "Yuengling & Son",
+        correctAnswer: "Yuengling Brewery",
         wrongAnswer1: "Sierra Nevada Brewing Co.",
         wrongAnswer2: "Stone Brewing Co.",
         wrongAnswer3: "Bell's Brewery"
@@ -94,6 +94,7 @@ var questionBankArr = [
 //FUNCTIONS
 //========================================================================================================================
 
+    //displays first question and advances to next question after userGuesses.
     function nextQuestion() {
         timer.reset();
         
@@ -101,13 +102,14 @@ var questionBankArr = [
            endGame();
        }
        else {
+            // resets/hides the post question specific items.
             $('#displayCorrectAnswer').text("");
             $('#roundExtras').hide();
-            $('#shuffle').show();    
-            $('.answerButton').prop('disabled', false);
+            $('#shuffle').show();
             timer.start();
             shuffleShuffle();
 
+            // loops through questionBank
             for(var i = questionNum; i < questionBankArr.length; i++){
                 $('#roundQuestion').text(questionBankArr[i].question);
                 $('.answer1').text(questionBankArr[i].correctAnswer);
@@ -120,10 +122,12 @@ var questionBankArr = [
         }
     }
 
+    //checks if user answer is correct or wrong.
     function checkAnswer(userAnswer, correct) {
         if(userAnswer === correct){
             totalCorrect++;
             $('#roundQuestion').text("Correct!");
+            //blocks wrong/noTime GIF, displays correct GIF
             $('.wrongImage').hide();
             $('.noTimeImage').hide();
             $('.correctImage').show();
@@ -133,46 +137,50 @@ var questionBankArr = [
             totalWrong++;
             $('#roundQuestion').text("Wrong!");
             $('#displayCorrectAnswer').text("Correct Answer: " + correct);
+            //blocks correct/noTime GIF, displays wrongGuess GIF
             $('.correctImage').hide();
             $('.noTimeImage').hide();
             $('.wrongImage').show();
         }
         $('#roundExtras').show();
         $("#timerDisplay").text(""); 
-        setTimeout(nextQuestion, SECOND * 4);
+        setTimeout(nextQuestion, SECOND * 3);
     }
 
 
-
+    //skips to next question if user runs out of question time.
     function noTimeLeft() {
         timer.stop();
 
         questionNum++;
         totalNoAnswer++;
 
+        //hides answers and display correct GIF
         $('#shuffle').hide();
         $('.wrongImage').hide();
         $('.correctImage').hide();
         $('#roundExtras').show();
         $('.noTimeImage').show(); 
 
+        //hides timer and displays out of time.
         $("#timerDisplay").text("");
-        $('.answerButton').prop('disabled', true);
         $('#roundQuestion').text("Out of time!");
-        setTimeout(nextQuestion, SECOND * 4);
+
+        //advances to next question after 4 seconds
+        setTimeout(nextQuestion, SECOND * 3);
     }
 
-
+    // ends game and displays round results.
     function endGame() {
         timer.stop();
-        $("#timerDisplay").text("");
-        //Display results after the game is over
-        // $('.answerButton').prop('disabled', true);
-        $('#shuffle').hide();
-        $('#roundExtras').hide(); 
-        $('#displayResults').show();
 
-        //print results
+        //hides game qeustion, answer, images (unwanted at game end)
+        $("#timerDisplay").text("");
+        $('#shuffle').hide();
+        $('#roundExtras').hide();
+
+        //displays results of round
+        $('#displayResults').show();
         $('.resultFinish').text("Game Over!");
         $('.resultCorrect').text("Correct: " + totalCorrect);
         $('.resultWrong').text("Wrong: " + totalWrong);
@@ -181,7 +189,7 @@ var questionBankArr = [
         //display new game button
         $('#roundQuestion').html('<button type="button" id="newGame" class="btn btn-light">New Game</button>');
         
-        //Resets the game on click
+        //Resets the game on newGame click
         $('#newGame').click(function(){
             resetVars();
             $('#displayResults').hide();
@@ -190,6 +198,7 @@ var questionBankArr = [
 
     }
 
+    // resets values to restart game without page refresh
     function resetVars(){
         $('.answerButton').prop('disabled', false);
         totalCorrect = 0;
@@ -198,6 +207,7 @@ var questionBankArr = [
         questionNum = 0;
     }
 
+    // shuffles the answer options in a different order each time.
     function shuffleShuffle(){
         var parent = $("#shuffle");
         var divs = parent.children();
@@ -209,6 +219,8 @@ var questionBankArr = [
 //========================================================================================================================
 // PLAY GAME
 //========================================================================================================================
+    
+    //at page load, hide shuffle and roundExtras divs.
     $('#shuffle').hide();
     $('#roundExtras').hide();
 
@@ -219,10 +231,10 @@ var questionBankArr = [
 
     $('.answerButton').click(function(){
          var userAnswer = $(this).text();
+         //saves the correctAnswer from question[i] as correct.
          var correct = nextQuestion();
          questionNum++;
          timer.stop();
-         $('.answerButton').prop('disabled', true);
         $('#shuffle').hide();
         checkAnswer(userAnswer, correct);
     });
